dbfaf8781d9d4529eafddc45dd70d4ddac9fc812
FlorianMgs <madgic.shrooms@gmail.com>
2021-10-25 00:14:27 +0200

    update - optimized algo

--- controllers/datasets.py
+++ controllers/datasets.py
@@ -8,7 +8,6 @@ def get_datasets() -> list:
     """
     path = './data/datasets/'
     datasets = [path + file for file in os.listdir(path)]
-    print(datasets)
     return datasets
 
 
@@ -16,14 +15,15 @@ def create_actions(path: str) -> list:
     """
     :param path: path to csv file containing actions
     :return: list of actions as tuples
+    Filter actions: remove actions where price <= 0
+                    remove duplicates
     """
     actions = []
     with open(path, newline='') as dataset_file:
         dataset = csv.DictReader(dataset_file)
         for row in dataset:
-            actions.append((row["name"], float(row["price"]), float(row["roi"])))
-
-    for action in actions:
-        print(action)
-    return actions
+            if float(row["price"]) > 0:
+                actions.append((row["name"], float(row["price"]), float(row["roi"])))
+        else:
+            return list(set(actions))
 
--- controllers/optimized.py
+++ controllers/optimized.py
@@ -7,36 +7,45 @@ def sort_actions(actions: list) -> list:
     """
     :param actions: list of actions
     :return: list of sorted actions
-    Calculate ratio ROI / price for each action.
-    Sort them in descending order.
+    Calculate earnings for each action.
+    Sort them by earnings in descending order.
     """
     with suppress(ZeroDivisionError):
-        actions = sorted(actions, key=lambda x: x[2] / x[1], reverse=True)
+        actions = sorted(actions, key=lambda x: x[1] * x[2] / 100, reverse=True)
     return actions
 
 
-def create_wallet(actions: list) -> Wallet:
+def add_actions(actions: list, wallet: Wallet) -> Wallet:
     """
     :param actions: list of actions
+    :param wallet: Wallet to populate with actions
     :return: most optimized wallet
     Iter all sorted actions, create the wallet with best ROI
     """
-    wallet = Wallet()
-    for action in actions:
-        if wallet["price"] < 500:
+
+    for i, action in enumerate(actions):
+        """
+        First, add best performing action
+        """
+        if i == 0:
             wallet.set_action(action)
-        else:
-            wallet.rem_action(index=-1)
-            break
-    wallet.set_earnings()
-    wallet.set_roi()
-    return wallet
+
+        """
+        Then, add actions with price < remaining cash to spend
+        """
+        if i > 0 and 0 < action[1] < 500 - wallet["price"]:
+            wallet.set_action(action)
+    else:
+        wallet.set_earnings()
+        wallet.set_roi()
+        return wallet
 
 
 def launch_optimized(actions):
     exec_time_start = time()
 
-    best_wallet = create_wallet(sort_actions(actions))
+    wallet = Wallet()
+    best_wallet = add_actions(sort_actions(actions), wallet)
 
     exec_time_end = time()
 
